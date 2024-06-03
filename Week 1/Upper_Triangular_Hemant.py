6002eda73556bbe598a7959bfb2e63f6ed20ba1f
Hemant <92645502+hemantkumar76@users.noreply.github.com>
2024-06-03 16:01:47 +0530

    Create Upper_Triangular_Hemant.py

---
+++ Week 1/Upper_Triangular_Hemant.py
@@ -0,0 +1,8 @@
+# Upper Triangle Wala Solved by Hemant
+def upper_triangular_wala(n):
+    for i in range(n, 0, -1):
+        print('*' * i)
+
+# number of rows dalenege
+n = 10
+upper_triangular_wala(n)
