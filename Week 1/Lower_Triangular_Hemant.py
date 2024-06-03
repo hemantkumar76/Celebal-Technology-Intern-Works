35cf2790096a8da6f0f9a0779eb4e29f53fa0232
Hemant <92645502+hemantkumar76@users.noreply.github.com>
2024-06-03 15:59:53 +0530

    Create Lower_Triangular_Hemant.py

---
+++ Week 1/Lower_Triangular_Hemant.py
@@ -0,0 +1,8 @@
+# Lower Triangle Wala Solved by Hemant
+def lower_triangular_wala(n):
+    for i in range(1, n + 1):
+        print('*' * i)
+
+# number of rows dalenege
+n = 10
+lower_triangular_wala(n)
